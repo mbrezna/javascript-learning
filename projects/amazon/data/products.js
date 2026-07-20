33935b9abe1bea669a41c26af66f75001fbda291
breznaM <breznamatej22@gmail.com>
2026-07-20 20:25:41 +0200

    Save data and generate HTML for payment summary

--- projects/amazon/data/products.js
+++ projects/amazon/data/products.js
@@ -1,3 +1,15 @@
+export function getProduct(cartProductId) {
+  let matchingProduct;
+
+  products.forEach((product) => {
+    if (product.id === cartProductId) {
+      matchingProduct = product;
+    }
+  });
+
+  return matchingProduct;
+}
+
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
